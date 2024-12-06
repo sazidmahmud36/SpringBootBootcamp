a41bdc34e8f763f89714b058787005a9764cbb0f
Sazid Mahmud <sazidmahmud36@gmail.com>
2024-12-06 11:35:38 +0600

    basic project

---
+++ springBootBootcamp/src/test/java/com/bootcamp/springBootBootcamp/SpringBootBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.springBootBootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
